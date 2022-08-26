bae183fdf4ac6e85a8ee3ce09eb52c72f43407a8
matheusrdss <62906049+matheusrdss@users.noreply.github.com>
2022-08-26 11:46:27 -0300

    a

--- organizerWhats/organizerWhats.js
+++ organizerWhats/organizerWhats.js
@@ -1,58 +1,25 @@
-let pessoasList = [
-    pessoaUm = {
-        itsMe: true,
-        admin: true,
-        name: 'Matheus'
-    },
-    pessoaDois = {
-        itsMe: false,
-        admin: false,
-        name: 'Nicolly'
-    },
-    pessoaTres = {
-        itsMe: false,
-        admin: true,
-        name: 'Jucelino'
-    }
-]
+var persons = [
+    {itsMe: true, admin: true, name: "Matheus"},
+    {itsMe: false, admin: false, name: "Nicolly"},
+    {itsMe: false, admin: true, name: "Guilherme"},
+    {itsMe: false, admin: false, name: "Arthur"},
+    {itsMe: false, admin: true, name: "Jucelino"},
+];
 
-const organizerWhats = (pessoasList) => {
-    let eu = undefined;
-    let adm = undefined;
-    let adm2 = undefined;
-    let adm3 = undefined;
-    let outro = undefined;
-    let outro2 = undefined;
-    let outro3 = undefined;
+const organizer = (personsList) => {
+    personsList.sort((a, b) => {
+        if (a.itsMe === true && b.itsMe === false) {
+            return-1;
+        }
+    });
 
-    // EU
-    if(pessoaUm.itsMe == true) {
-        eu = pessoaUm.name;
-    } else if (pessoaDois.itsMe == true) {
-        eu = pessoaDois.name;
-    } else if (pessoaTres.itsMe == true) {
-        eu = pessoaTres.name;
-    }
+    personsList.sort((a, b) => {
+        if (a.admin === true && b.admin === false) {
+            return-1;
+        }
+    });
 
-    // ADM
-    if(pessoaUm.adm == true) {
-        adm = pessoaUm.name;
-    } else if (pessoaDois.adm == true) {
-        eu = pessoaDois.name;
-    } else if (pessoaTres.itsMe == true) {
-        eu = pessoaTres.name;
-    }
+    console.log(personsList);
+};
 
-    // OUTRO
-    if(pessoaUm.itsMe == true) {
-        eu = pessoaUm.name;
-    } else if (pessoaDois.itsMe == true) {
-        eu = pessoaDois.name;
-    } else if (pessoaTres.itsMe == true) {
-        eu = pessoaTres.name;
-    }
-
-    console.log(eu);
-}
-
-organizerWhats(pessoasList);
+organizer(persons);
